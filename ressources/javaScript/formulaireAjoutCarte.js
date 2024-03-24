ad60ad766fc6209048a9b2325870f4b7577a7bce
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-03-24 22:25:24 +0100

    feat : fonctions ajourParticipant et supprimerParticipant sur carte OK, reste API

--- ressources/javaScript/formulaireAjoutCarte.js
+++ ressources/javaScript/formulaireAjoutCarte.js
@@ -29,7 +29,7 @@ let formulaireAjoutCarte = reactive({
                 titreCarte: this.titre,
                 descriptifCarte: this.description,
                 couleurCarte: this.couleur,
-                affectationsCarte: []
+                affectationsCarte: this.participants
             })
         });
 
@@ -89,24 +89,27 @@ let formulaireAjoutCarte = reactive({
             if (membres.length === 0) {
                 return '<p>Il n\'y a pas de collaborateurs pour le moment</p><span class="addCollborateurs">Ajouter des collaborateurs</span>\n';
             }
+            setTimeout(() => {
+                startReactiveDom();
+            }, 100);
             membres.forEach(membre => {
-                return document.querySelector('.listeParticipants').innerHTML +=  `<input data-onUncheck="formulaireAjoutCarte.supprimerParticipantCarte(${membre.login})" type="checkbox" data-participant="${membre.login}" id="participant${membre.login}" name="participant${membre.login}" value="${membre.login}">
+                return document.querySelector('.listeParticipants').innerHTML +=  `<input data-onUncheck="formulaireAjoutCarte.supprimerParticipantCarte(${membre.login})" data-oncheck="formulaireAjoutCarte.ajouterParticipantCarte(${membre.login})" type="checkbox" data-participant="${membre.login}" id="participant${membre.login}" name="participant${membre.login}" value="${membre.login}">
                 <label for="participant${membre.login}" data-participant="${membre.login}"><span class="user">${membre.prenom[0]}${membre.nom[0]}</span></label>`;
             });
         }
-
-
     },
 
-    ajouterParcipantCarte: function (idUtilisateur) {
-        if (this && this.participants) {
+    ajouterParticipantCarte: function (idUtilisateur) {
+        if (this && this.participants && !this.participants.includes(idUtilisateur)) {
             this.participants.push(idUtilisateur);
+            console.log(this.participants);
         }
     },
 
     supprimerParticipantCarte: function (idUtilisateur) {
         if (this && this.participants) {
             this.participants = this.participants.filter(participant => participant !== idUtilisateur);
+            console.log(this.participants);
         }
     }
 
