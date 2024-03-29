b13286c026cf02bdf37161df00f90001d5a9cff0
Thomas Loye <thomas.loye@etu.umontpellier.fr>
2024-03-29 11:00:31 +0100

    feat : avancées

--- ressources/javaScript/utilisateurs.js
+++ ressources/javaScript/utilisateurs.js
@@ -45,7 +45,52 @@ let utilisateurs = reactive({
             let proprio = await proprioTableau.json();
             const tousMembres = membres.concat(proprio);
 
-            console.log(JSON.stringify(tousMembres, null, 2));
+
+            for (let membre of tousMembres) {
+                let utilisateur = Object.create(this);
+                utilisateur.prenom = membre.prenom;
+                utilisateur.nom = membre.nom;
+                utilisateur.login = membre.login;
+                utilisateur.colonnes = [];
+                utilisateur.drapeau = false;
+
+                console.log(utilisateur);
+
+                let colonnes = document.querySelectorAll('.draggable');
+
+                for (let colonne of colonnes) {
+                    let nbCartes = await fetch(apiBase + '/utilisateur/affectations', {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            idColonne: colonne.getAttribute('data-colonne')
+                        })
+                    });
+
+                    if (nbCartes.status !== 200) {
+                        console.error(nbCartes.json());
+                    } else {
+                        /**
+                         * Retourne
+                         * "loginUtilisateur": {
+                         *         "colonnes": {
+                         *             "idColonne": [
+                         *                 "titreColonne",
+                         *                 nbCartes
+                         *             ]
+                         *         }
+                         *     },
+                         */
+                        let nbCartesJson = await nbCartes.json();
+
+                        console.log(JSON.stringify(nbCartesJson, null, 2));
+                    }
+                }
+
+            }
 
             this.drapeau = false;
         }
